eb84df059811c4b5ccc24fa70e3cb800255d614f
Alexander Goncharov <luzlol17@gmail.com>
2018-05-13 01:22:42 +0500

    Версия 0.8.0 — фикс подключения к БД.

--- service/GKP7/config/index.js
+++ service/GKP7/config/index.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 module.exports = {
   secret: process.env.SECRET_KEY,
   session: {session: false},
-  database: 'mongodb://localhost/gkp7'
+  database: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/gkp7?authSource=${process.env.DB_SOURCE}`
 }
